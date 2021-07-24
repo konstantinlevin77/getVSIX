cd1543e0e8cd9270e93fa41edd7d8a27b6af5131
Mehmet Yaşar Tekman <60093612+konstantinlevin77@users.noreply.github.com>
2021-07-24 13:32:55 +0000

    Default config creator has been written.

---
+++ create_config.py
@@ -0,0 +1,16 @@
+import pathlib
+
+default_config = """# You can sort the results by Relevance | Installs | Updated Date | Publisher | Name
+# | Published Date | Rating | Trending
+SortBy=Installs
+
+# You can determine how many results do you want to see, it can be 10 maximum.
+Top=5
+"""
+
+def create_default_config():
+    file_path = pathlib.Path("config")
+    print(not(file_path.is_file() and file_path.exists()))
+    if not(file_path.is_file() and file_path.exists()):
+        with open("config",mode="w") as F:
+            F.writelines(default_config)
